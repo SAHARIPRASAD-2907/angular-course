8bb4d0c15431b2cb7a1965de218eff8fd07c879a
HARIPRSAD SIVAPATHAM ANAND <67941813+SAHARIPRASAD-2907@users.noreply.github.com>
2024-03-02 19:37:10 -0400

    using server component

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  //selector: '.app-servers',
+  // selector: '.app-servers',
+  selector: 'app-server',
   templateUrl: './servers.component.html',
   styleUrl: './servers.component.css'
 })
